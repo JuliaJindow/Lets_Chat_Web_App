fd7e6cca7f6b299b3ce6ba31b249254bfdf75faa
Julia <74714789+JuliaJindow@users.noreply.github.com>
2021-04-22 13:03:47 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,16 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+      apiKey: "AIzaSyD1VXZYtj0Hca6Mym1d7_xAaYZprwGuDH4",
+      authDomain: "p-94-database.firebaseapp.com",
+      projectId: "p-94-database",
+      storageBucket: "p-94-database.appspot.com",
+      messagingSenderId: "943332840228",
+      appId: "1:943332840228:web:55274fb2f4ab68fdd219b1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+      //Start code
+    
+      //End code
